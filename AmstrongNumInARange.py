04df15bfd44863a1f0bfb40572f15ab648e117a5
Reinhard kenson <kenson.reinhard90@gmail.com>
2019-04-12 19:11:27 +0530

    Update AmstrongNumInARange.py

--- AmstrongNumInARange.py
+++ AmstrongNumInARange.py
@@ -1,4 +1,4 @@
-class amstrongnum:
+class amstrongnumr:
     def __init__(self):
         self.numchar=input()
         self.numchar=[int(i) for i in (self.numchar).split(" ")]
@@ -27,5 +27,5 @@ class amstrongnum:
                 print(lst[i],end=" ")
             
 
-cd=amstrongnum()
+cd=amstrongnumr()
 cd.findamstrong()
