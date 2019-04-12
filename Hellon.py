1a4bf7acdd4a4e5b16f9b9ea0cd9d9166b3205e3
Reinhard kenson <kenson.reinhard90@gmail.com>
2019-04-12 18:53:01 +0530

    Update Hellon.py

--- Hellon.py
+++ Hellon.py
@@ -1,9 +1,9 @@
-class Hello:
+class Hellon:
     def __init__(self):
         self.n= int(input())
     def printing(self):
         for i in range(0,self.n):
             print("Hello")
 
-h= Hello()
+h= Hellon()
 h.printing()
