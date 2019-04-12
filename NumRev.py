4044240ec72985b289689a0eaac5ee21933cde8b
Reinhard kenson <kenson.reinhard90@gmail.com>
2019-04-12 20:33:23 +0530

    Update NumRev.py

--- NumRev.py
+++ NumRev.py
@@ -1,10 +1,10 @@
-class NumRev:
+class NumRever:
     def __init__(self):
         self.n=input()
     def Rev(self):
         self.n=self.n[::-1]
         print(int(self.n))
 
-n=NumRev()
+n=NumRever()
 n.Rev()
             
